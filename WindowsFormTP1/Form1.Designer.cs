1b5a47c5faf3e97fd4faae8bfc449528e4109864
slevalegant <slevalegant@E207-8>
2025-01-17 12:11:31 +0100

    TP1

--- WindowsFormTP1/Form1.Designer.cs
+++ WindowsFormTP1/Form1.Designer.cs
@@ -28,13 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_afficher = new System.Windows.Forms.TextBox();
+            this.lbl_rep = new System.Windows.Forms.Label();
+            this.btn_valider = new System.Windows.Forms.Button();
+            this.lbl_Nom = new System.Windows.Forms.Label();
+            this.lbl_Prenom = new System.Windows.Forms.Label();
+            this.txt_Nom = new System.Windows.Forms.TextBox();
+            this.txt_Prenom = new System.Windows.Forms.TextBox();
+            this.lbl_Difficulte = new System.Windows.Forms.Label();
+            this.comboBox_difficulté = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txt_afficher
+            // 
+            this.txt_afficher.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txt_afficher.Enabled = false;
+            this.txt_afficher.Location = new System.Drawing.Point(468, 168);
+            this.txt_afficher.Name = "txt_afficher";
+            this.txt_afficher.Size = new System.Drawing.Size(339, 20);
+            this.txt_afficher.TabIndex = 0;
+            // 
+            // lbl_rep
+            // 
+            this.lbl_rep.AutoSize = true;
+            this.lbl_rep.Location = new System.Drawing.Point(401, 171);
+            this.lbl_rep.Name = "lbl_rep";
+            this.lbl_rep.Size = new System.Drawing.Size(50, 13);
+            this.lbl_rep.TabIndex = 1;
+            this.lbl_rep.Text = "Réponse";
+            // 
+            // btn_valider
+            // 
+            this.btn_valider.Location = new System.Drawing.Point(521, 363);
+            this.btn_valider.Name = "btn_valider";
+            this.btn_valider.Size = new System.Drawing.Size(75, 23);
+            this.btn_valider.TabIndex = 2;
+            this.btn_valider.Text = "Push Me";
+            this.btn_valider.UseVisualStyleBackColor = true;
+            this.btn_valider.Click += new System.EventHandler(this.btn_valider_Click);
+            // 
+            // lbl_Nom
+            // 
+            this.lbl_Nom.AutoSize = true;
+            this.lbl_Nom.Location = new System.Drawing.Point(401, 221);
+            this.lbl_Nom.Name = "lbl_Nom";
+            this.lbl_Nom.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Nom.TabIndex = 3;
+            this.lbl_Nom.Text = "Nom :";
+            // 
+            // lbl_Prenom
+            // 
+            this.lbl_Prenom.AutoSize = true;
+            this.lbl_Prenom.Location = new System.Drawing.Point(401, 275);
+            this.lbl_Prenom.Name = "lbl_Prenom";
+            this.lbl_Prenom.Size = new System.Drawing.Size(49, 13);
+            this.lbl_Prenom.TabIndex = 4;
+            this.lbl_Prenom.Text = "Prénom :";
+            // 
+            // txt_Nom
+            // 
+            this.txt_Nom.Location = new System.Drawing.Point(507, 218);
+            this.txt_Nom.Name = "txt_Nom";
+            this.txt_Nom.Size = new System.Drawing.Size(124, 20);
+            this.txt_Nom.TabIndex = 5;
+            // 
+            // txt_Prenom
+            // 
+            this.txt_Prenom.Location = new System.Drawing.Point(507, 272);
+            this.txt_Prenom.Name = "txt_Prenom";
+            this.txt_Prenom.Size = new System.Drawing.Size(124, 20);
+            this.txt_Prenom.TabIndex = 6;
+            // 
+            // lbl_Difficulte
+            // 
+            this.lbl_Difficulte.AutoSize = true;
+            this.lbl_Difficulte.Location = new System.Drawing.Point(401, 323);
+            this.lbl_Difficulte.Name = "lbl_Difficulte";
+            this.lbl_Difficulte.Size = new System.Drawing.Size(57, 13);
+            this.lbl_Difficulte.TabIndex = 7;
+            this.lbl_Difficulte.Text = "Difficulté : ";
+            // 
+            // comboBox_difficulté
+            // 
+            this.comboBox_difficulté.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_difficulté.FormattingEnabled = true;
+            this.comboBox_difficulté.Location = new System.Drawing.Point(507, 320);
+            this.comboBox_difficulté.Name = "comboBox_difficulté";
+            this.comboBox_difficulté.Size = new System.Drawing.Size(124, 21);
+            this.comboBox_difficulté.TabIndex = 8;
+            this.comboBox_difficulté.SelectedIndexChanged += new System.EventHandler(this.comboBox_difficulté_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1177, 723);
+            this.Controls.Add(this.comboBox_difficulté);
+            this.Controls.Add(this.lbl_Difficulte);
+            this.Controls.Add(this.txt_Prenom);
+            this.Controls.Add(this.txt_Nom);
+            this.Controls.Add(this.lbl_Prenom);
+            this.Controls.Add(this.lbl_Nom);
+            this.Controls.Add(this.btn_valider);
+            this.Controls.Add(this.lbl_rep);
+            this.Controls.Add(this.txt_afficher);
+            this.Name = "Form1";
+            this.Text = "Mon 1er formulaire ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_afficher;
+        private System.Windows.Forms.Label lbl_rep;
+        private System.Windows.Forms.Button btn_valider;
+        private System.Windows.Forms.Label lbl_Nom;
+        private System.Windows.Forms.Label lbl_Prenom;
+        private System.Windows.Forms.TextBox txt_Nom;
+        private System.Windows.Forms.TextBox txt_Prenom;
+        private System.Windows.Forms.Label lbl_Difficulte;
+        private System.Windows.Forms.ComboBox comboBox_difficulté;
     }
 }
 
--- WindowsFormTP1/Form1.cs
+++ WindowsFormTP1/Form1.cs
@@ -15,6 +15,50 @@ namespace WindowsFormTP1
         public Form1()
         {
             InitializeComponent();
+            comboBox_difficulté.Items.Add("Facile");
+            comboBox_difficulté.Items.Add("Moyen");
+            comboBox_difficulté.Items.Add("Difficile");
+            comboBox_difficulté.Items.Add("Enfer");
         }
+
+        private void btn_valider_Click(object sender, EventArgs e)
+        {
+            
+            if (txt_Nom.Text !="" && txt_Prenom.Text !="")
+            {
+                string result = "Bonjour ";
+                result += txt_Prenom.Text + " " + txt_Nom.Text + "\r\n La difficultés sélectionnée est: " + comboBox_difficulté.SelectedItem;
+                txt_afficher.Text = result;
+
+            }
+            else
+            {
+                MessageBox.Show("Aucun nom ou prénom n'est rentré", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+            
+        }
+
+        private void comboBox_difficulté_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox_difficulté.SelectedItem == "Enfer")
+            {
+                this.BackColor = Color.Red;
+                lbl_rep.Font = new Font(lbl_rep.Font, FontStyle.Bold);
+                lbl_Nom.Font = new Font(lbl_Nom.Font, FontStyle.Bold);
+                lbl_Prenom.Font = new Font(lbl_Prenom.Font, FontStyle.Bold);
+                lbl_Difficulte.Font = new Font(lbl_Difficulte.Font, FontStyle.Bold);
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(153, 180, 209);
+                lbl_rep.Font = new Font(lbl_rep.Font, FontStyle.Regular);
+                lbl_Nom.Font = new Font(lbl_Nom.Font, FontStyle.Regular);
+                lbl_Prenom.Font = new Font(lbl_Prenom.Font, FontStyle.Regular);
+                lbl_Difficulte.Font = new Font(lbl_Difficulte.Font, FontStyle.Regular);
+            }
+        }
+
+        
     }
 }
